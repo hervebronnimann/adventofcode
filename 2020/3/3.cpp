29f4ea6949671f840a73d63669135b820002308b
Herve Bronnimann <hervebronnimann@mac.com>
2020-12-03 01:24:26 -0800

    Clean up day 3.

--- 2020/3/3.cpp
+++ 2020/3/3.cpp
@@ -5,6 +5,25 @@
 
 std::vector<std::string> trees;
 
+bool loadTrees(const char* filename)
+{
+  std::ifstream input(filename);
+  if (!input) {
+    return false;
+  }
+  std::string line; int len = 0, k = 0;
+  while (std::getline(input, line)) {
+    ++k;
+    if (len == 0) len = line.length();
+    if (line.length() != len) {
+      std::cout << "Wrong length! " << line.length() << " len " << len << " line " << k << std::endl;
+      return false;
+    }
+    trees.push_back(line);
+  }
+  return true;
+}
+
 int numTrees(int right, int down)
 {
   int n = 0, pos = 0, k = 0, len = 0;
@@ -21,23 +40,11 @@ int numTrees(int right, int down)
 }
 
 int main(int argc, char **argv) {
-  std::ifstream input("trees.txt");
-  if (!input) {
-    std::cerr << "Couldn't open trees.txt" << std::endl;
+  auto filename = argc > 1 ? argv[1] : "trees.txt";
+  if (!loadTrees(filename)) {
+    std::cerr << "Couldn't open " << filename << std::endl;
     return -1;
   }
-
-  std::string line; int len = 0, k = 0;
-  while (std::getline(input, line)) {
-    ++k;
-    if (len == 0) len = line.length();
-    if (line.length() != len) {
-      std::cout << "Wrong length! " << line.length() << " len " << len << " line " << k << std::endl;
-      return -1;
-    }
-    trees.push_back(line);
-  }
-
   int n = 1;
   n *= numTrees(1, 1);
   n *= numTrees(3, 1);  // this is first half
