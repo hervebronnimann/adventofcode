842347a01d2c6af3bd451d03b63bb8178d7e4b3f
Herve Bronnimann <hervebronnimann@mac.com>
2021-12-08 10:21:36 -0800

    Tweaking python...

--- 2021/8/8.1.py
+++ 2021/8/8.1.py
@@ -1,10 +1,8 @@
+import numpy as np
+
 output=[]
 with open('input.txt') as f:
   for line in f:
     output.append(line.split('|')[1].split())
 
-n=0
-for x in output:
-  for s in x:
-    if len(s) in [2,3,4,7]: n += 1
-print(n)
+print(np.sum([np.sum([1 for s in x if len(s) in [2,3,4,7]]) for x in output]))
