0525d063ad922eee31c4db00fe548b274947e295
Herve Bronnimann <hervebronnimann@mac.com>
2021-12-09 18:19:17 -0800

    More esoteric python (but easier to extend to diagonal neighbors) for day 9.

--- 2021/9/9.1.py
+++ 2021/9/9.1.py
@@ -1,14 +1,11 @@
 def digit(x): return ord(x)-ord('0')
 
 with open("input.txt") as f:
-  m=0; b={}
-  for line in f: n=len(line); m+=1; b[m]='9'+line.rstrip('\n')+'9'
-  b[0]='9'*(n+2); b[m+1]='9'*(n+2)
+  b=['9'+line.rstrip('\n')+'9' for line in f]
+  m=len(b); n=len(b[0])-2
+  b.insert(0,'9'*(n+2)); b.append('9'*(n+2))
 
-risk=0
-for i in range(1,m+1):
-  for j in range(1,n+1):
-    if b[i][j]<b[i+1][j] and b[i][j]<b[i-1][j] and b[i][j]<b[i][j+1] and b[i][j]<b[i][j-1]:
-      # print("Low point %s at [%d,%d]" % (b[i][j],i,j))
-      risk += digit(b[i][j])+1
-print(risk)
+# print("\n".join(b))
+neighbors=[(-1,0),(1,0),(0,1),(0,-1)]
+print(sum(digit(b[i][j])+1  for i in range(1,m+1) for j in range(1,n+1) \
+      if all([b[i][j]<b[i+u][j+v] for (u,v) in neighbors])))
--- 2021/9/9.2.py
+++ 2021/9/9.2.py
@@ -1,16 +1,17 @@
 def digit(x): return ord(x)-ord('0')
 
 with open("input.txt") as f:
-  m=0; b={}
-  for line in f: n=len(line); m+=1; b[m]='9'+line.rstrip('\n')+'9'
-  b[0]='9'*(n+2); b[m+1]='9'*(n+2)
+  b=['9'+line.rstrip('\n')+'9' for line in f]
+  m=len(b); n=len(b[0])-2
+  b.insert(0,'9'*(n+2)); b.append('9'*(n+2))
+
+neighbors=[(-1,0),(1,0),(0,1),(0,-1)]
 
 # Find low points, seed basins
 k=0; basin=[[0 for i in range(n+2)] for j in range(m+2)]; size=[0]
 for i in range(1,m+1):
   for j in range(1,n+1):
-    if b[i][j]<b[i+1][j] and b[i][j]<b[i-1][j] and b[i][j]<b[i][j+1] and b[i][j]<b[i][j-1]:
-      # print("Low point %s at [%d,%d]" % (b[i][j],i,j))
+    if all([b[i][j]<b[i+u][j+v] for (u,v) in neighbors]):
       k += 1; basin[i][j] = k; size.append(1)
 
 # Propagate basins by neighbors
@@ -20,10 +21,10 @@ while not finished:
   for i in range(1,m+1):
     for j in range(1,n+1):
       if digit(b[i][j])==9 or basin[i][j]>0: continue
-      if   basin[i+1][j]>0: basin[i][j]=basin[i+1][j]; size[basin[i][j]] += 1; finished=False
-      elif basin[i-1][j]>0: basin[i][j]=basin[i-1][j]; size[basin[i][j]] += 1; finished=False
-      elif basin[i][j+1]>0: basin[i][j]=basin[i][j+1]; size[basin[i][j]] += 1; finished=False
-      elif basin[i][j-1]>0: basin[i][j]=basin[i][j-1]; size[basin[i][j]] += 1; finished=False
+      for (u,v) in neighbors:
+        if basin[i+u][j+v]>0:
+          basin[i][j]=basin[i+u][j+v]; size[basin[i][j]] += 1
+          finished=False; break
 
 size.sort(reverse=True)
 print(size[0] * size[1] * size[2])
