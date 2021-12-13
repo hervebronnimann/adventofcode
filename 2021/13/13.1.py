dafed67aac2d97d16855a6a91d0338ff25db560a
Herve Bronnimann <hervebronnimann@mac.com>
2021-12-12 21:44:21 -0800

    My python is improving.

--- 2021/13/13.1.py
+++ 2021/13/13.1.py
@@ -1,9 +1,9 @@
 dots={}
-with open("example.txt") as f:
+with open("input.txt") as f:
   for l in f:
     if l=='\n': break
     x=l.rstrip('\n').split(',')
-    dots[(int(x[0]),int(x[1]))]=1
+    dots[tuple(map(int,x))]=1
   iter=0
   for l in f:
     iter += 1
@@ -17,19 +17,9 @@ with open("example.txt") as f:
         if j>t: del dots[(i,j)]; dots[(i,2*t-j)]=1
     print("Iter: %d dots:%d" % (iter,len(dots)))
 
-
-x=[]; y=[]
-for i,j in dots.keys():
-  x.append(i)
-  y.append(j)
-xmax=max(x)
-ymax=max(y)
-print(xmax)
-print(ymax)
-print(dots)
-
-mat=[['.' for i in range(xmax+1)] for j in range(ymax+1)]
-for i,j in dots.keys():
-  mat[j][i]='#'
-for i in range(ymax+1):
+x,y = zip(*dots.keys())
+mat=[[('#' if (i,j) in dots.keys() else '.')
+      for i in range(max(x)+1)]
+      for j in range(max(y)+1)]
+for i in range(max(y)+1):
   print("".join(mat[i]))
