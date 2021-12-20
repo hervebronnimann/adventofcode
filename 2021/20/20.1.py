c88ca89528c0843266c73559354cb72e90b26996
Herve Bronnimann <hervebronnimann@mac.com>
2021-12-19 22:38:49 -0800

    Fix it - with the correct padding - ugh.  This was not so much fun...

--- 2021/20/20.1.py
+++ 2021/20/20.1.py
@@ -3,38 +3,41 @@ from functools import reduce
 def btod(str): return reduce(lambda x,y: 2*x+int(y), str, 0)
 def atos(array): return reduce(lambda x,y: x+y, array, '')
 
-def neighbor(s,i,j):
-  def helper(i,j):
-    m=len(s); n=len(s[0])
-    if i<=1 or i>=m+2: return '...'
-    if j==0:   return '..'+s[i-2][0:1]
-    if j==1:   return '.'+s[i-2][0:2]
-    if j==n+2: return s[i-2][n-2:n]+'.'
-    if j==n+3: return s[i-2][n-1:n]+'..'
-    return s[i-2][j-2:j+1]
-  t=helper(i,j)+helper(i+1,j)+helper(i+2,j)
-  return t.replace('.','0').replace('#','1')
 
 def p(s):
   print('   ------')
   for l in s: print(l)
   print('   ------')
 
-def decode(s,pattern):
-  m=len(s)+4; n=len(s[0])+4
-  o=[[pattern[btod(neighbor(s,i,j))] for j in range(n)] for i in range(m)]
-  return [atos(o[i]) for i in range(m)] 
+def pad(s,c,p):
+  for i in range(len(s)):
+     for j in range(p): s[i]=c+s[i]+c
+  empty=atos([c for j in range(len(s[0]))])
+  for j in range(p):
+    s.insert(0, empty); s.append(empty)
+
+def decode(s,m,n,pattern):
+  def neighbor(s,i,j):
+    i += 1; j += 1
+    return (s[i-1][j-1:j+2]+s[i][j-1:j+2]+s[i+1][j-1:j+2]).replace('.','0').replace('#','1')
+  return [atos([pattern[btod(neighbor(s,i,j))] for j in range(n+2)]) for i in range(m+2)]
 
 def solve(filename):
   s=[]
   with open(filename) as f:
     pattern=next(f).strip()
+    c=pattern[0]
     next(f)
-    for l in f:
-      s.append(l.strip())
-    p(s)
-    s=decode(s,pattern); p(s)
-    s=decode(s,pattern); p(s)
+    for l in f: s.append(l.strip())
+    # First iteration, padding with '.'
+    m=len(s); n=len(s[0])
+    pad(s,'.',2); p(s)
+    s=decode(s,m,n,pattern)
+    # Next iteration, this time padding with c, not '.'
+    m=len(s); n=len(s[0])
+    pad(s,c,2); p(s)
+    s=decode(s,m,n,pattern);
+    pad(s,'.',2); p(s)
     count=reduce(lambda x,y: x+y.count('#'), s, 0)
     print('SOLVED: %d for %s' % (count, filename))
 
