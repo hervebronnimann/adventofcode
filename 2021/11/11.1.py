9e106dd6540206287363f682c41d47482357c82c
Herve Bronnimann <hervebronnimann@mac.com>
2021-12-19 22:44:02 -0800

    Doing 50 iterations - phew it worked out of the box...

--- 2021/11/11.1.py
+++ 2021/11/11.1.py
@@ -1,6 +1,7 @@
 neighbors=[(i,j) for i in [-1,0,1] for j in [-1,0,1] if i!=0 or j!=0]
 def grid(m,n): return [(i,j) for i in range(n) for j in range(m)]
 def valid(i,j,b): return i>=0 and i<len(b) and j>=0 and j<len(b[0])
+<<<<<<< Updated upstream
 
 with open("input.txt") as f:
   b=[[ord(x)-ord('0') for x in line.rstrip('\n')] for line in f]
@@ -10,6 +11,17 @@ def flash(i,j,b):
   for u,v in neighbors:
     if valid(i+u,j+v,b) and b[i+u][j+v]<10:
       b[i+u][j+v] += 1
+=======
+def digit(x): return ord(x)-ord('0')
+
+with open("input.txt") as f:
+  b=[[digit(x) for x in line.rstrip('\n')] for line in f]
+
+def flash(b,i,j):
+  if b[i][j]!=10: return 0
+  for u,v in neighbors:
+    if valid(i+u,j+v,b): b[i+u][j+v] += 1
+>>>>>>> Stashed changes
   b[i][j] = 99  # To prevent flashing twice
   return 1
 
@@ -19,7 +31,11 @@ for iter in range(100):
   for i,j in g: b[i][j]+=1
   flashes = 0;
   while True:
+<<<<<<< Updated upstream
      f=sum([flash(i,j,b) for i,j in g])
+=======
+     f=sum([flash(b,i,j) for i,j in g])
+>>>>>>> Stashed changes
      if f==0: break
      flashes += f
   for i,j in g:
---
+++ 2021/20/20.2.py
@@ -0,0 +1,46 @@
+from functools import reduce
+
+def btod(str): return reduce(lambda x,y: 2*x+int(y), str, 0)
+def atos(array): return reduce(lambda x,y: x+y, array, '')
+
+
+def p(s):
+  print('   ------')
+  for l in s: print(l)
+  print('   ------')
+
+def pad(s,c,p):
+  for i in range(len(s)):
+     for j in range(p): s[i]=c+s[i]+c
+  empty=atos([c for j in range(len(s[0]))])
+  for j in range(p):
+    s.insert(0, empty); s.append(empty)
+
+def decode(s,m,n,pattern):
+  def neighbor(s,i,j):
+    i += 1; j += 1
+    return (s[i-1][j-1:j+2]+s[i][j-1:j+2]+s[i+1][j-1:j+2]).replace('.','0').replace('#','1')
+  return [atos([pattern[btod(neighbor(s,i,j))] for j in range(n+2)]) for i in range(m+2)]
+
+def solve(filename):
+  s=[]
+  with open(filename) as f:
+    pattern=next(f).strip()
+    c=pattern[0]
+    next(f)
+    for l in f: s.append(l.strip())
+    for i in range(25):
+      # First iteration, padding with '.'
+      m=len(s); n=len(s[0])
+      pad(s,'.',2); # p(s)
+      s=decode(s,m,n,pattern)
+      # Next iteration, this time padding with c, not '.'
+      m=len(s); n=len(s[0])
+      pad(s,c,2); # p(s)
+      s=decode(s,m,n,pattern);
+      pad(s,'.',2); # p(s)
+    count=reduce(lambda x,y: x+y.count('#'), s, 0)
+    print('SOLVED: %d for %s' % (count, filename))
+
+solve('example.txt')
+solve('input.txt')
