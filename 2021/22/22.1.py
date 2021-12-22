003a9aa2d0d5108c5dc64f95a2a38aa8576b2b6e
Herve Bronnimann <hervebronnimann@mac.com>
2021-12-21 23:54:05 -0800

    Also cleanup of part 1

--- 2021/22/22.1.py
+++ 2021/22/22.1.py
@@ -1,9 +1,7 @@
-from functools import reduce
+from collections import defaultdict
 
-def rsum(a): reduce(lambda x,y: x+rsum(y), a, 0)
-
+grid=defaultdict(int)
 with open("input.txt") as f:
-  grid=[[[0 for i in range(-50,51)] for j in range(-50,51)] for k in range(-50,51)]
   for l in f:
     s,x1,x2,y1,y2,z1,z2=[int(n) for n in l.strip().split()]
     if x1<-50 or x2>50: continue
@@ -12,10 +10,5 @@ with open("input.txt") as f:
     for x in range(x1,x2+1):
       for y in range(y1,y2+1):
         for z in range(z1,z2+1):
-          grid[x+50][y+50][z+50] = s
-  sum=0
-  for x in range(-50,51):
-    for y in range(-50,51):
-      for z in range(-50,51):
-        sum += grid[x+50][y+50][z+50]
-  print(sum)
+          grid[(x,y,z)] = s
+  print(sum(grid.values()))
