d395ec1b6cea2be80696f45ed63a1a3cef7e6b76
Herve Bronnimann <hervebronnimann@mac.com>
2022-12-16 21:03:17 -0800

    Improvements...

--- 2022/16/16.1.py
+++ 2022/16/16.1.py
@@ -10,7 +10,7 @@ flow  = [ int(x.split(' ')[5]) for x in lines ]
 dest = [ x.split(' ')[10:] for x in lines ]
 dest = dict(zip(nodes,dest))
 
-# Compacting the non-zero nodes, by all-pairs
+# Compacting the non-zero nodes, by all-pairs (Floyd-Warshall)
 
 Dist={}
 for x in nodes:
@@ -26,26 +26,37 @@ for n in range(len(nodes)):
   if Dist==Dist2: break
   Dist=Dist2
 
-# now solving problem
+# and indexing with non-zero flow nodes from 1 to N
 
+index=0
 Nodes=[]
+Index={'AA':0}
 Flow={}
 for n,f in zip(nodes,flow):
-  if f!=0: Nodes.append(n); Flow[n]=f
-
+  if f!=0: index += 1; Nodes.append(index); Flow[index]=f; Index[n]=index
 print(Nodes)
-# print(Dist)
+print(Flow)
 
-@lru_cache
+IDist = [[0 for _ in range(index+1)] for _ in range(index+1)]
+for n1,f1 in zip(nodes,flow):
+  if n1!='AA' and f1==0: continue
+  for n2,f2 in zip(nodes,flow):
+    if n2!='AA' and f2==0: continue
+    IDist[Index[n1]][Index[n2]] = Dist[(n1,n2)]
+print(IDist)
+
+# now solving problem
+
+# @lru_cache
 def explore(minute,node,open_nodes,num_open_nodes):
   if minute<=1 or num_open_nodes==len(Nodes): return 0
   ans = 0
   for x in Nodes:
     if x != node and (2**Nodes.index(x))&open_nodes==0:
-      ans = max(ans,explore(minute-1-Dist[(node,x)],x,open_nodes|(2**Nodes.index(node)),num_open_nodes+1))
+      ans = max(ans,explore(minute-1-IDist[node][x],x,open_nodes|(2**Nodes.index(node)),num_open_nodes+1))
   return (minute-1)*Flow[node] + ans
 
 ans=0
 for x in Nodes:
-  ans=max(ans,explore(30-Dist[('AA',x)],x,0,0))
+  ans=max(ans,explore(30-IDist[0][x],x,0,0))
 print(ans)
