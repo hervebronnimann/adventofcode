a1b73be0975e5725d4a1be5d488010d162c7e73d
Herve Bronnimann <hervebronnimann@mac.com>
2022-12-18 20:58:54 -0800

    More cleanup

--- 2022/18/18.1.py
+++ 2022/18/18.1.py
@@ -1,6 +1,6 @@
 def parse(l): x,y,z=l.split(','); return (1+int(x),1+int(y),1+int(z))
 lines = list(map(parse, open("input.txt").read().strip().split('\n')))
 X = [max([t[k]+1 for t in lines]) for k in range(3)]
-dX = [[set([tuple(t[:k]+t[k+1:]) for t in filter(lambda t: t[k]==x, lines)]) for x in range(X[k]+1)] for k in range(3)]
+dX = [[set([tuple(t[:k]+t[k+1:]) for t in lines if t[k]==x]) for x in range(X[k]+1)] for k in range(3)]
 ans = sum([sum([len(dX[k][x].symmetric_difference(dX[k][x-1])) for x in range(1,X[k]+1)]) for k in range(3)])
 print(ans)
--- 2022/18/18.2.py
+++ 2022/18/18.2.py
@@ -4,7 +4,7 @@ def parse(l): x,y,z=l.split(','); return (1+int(x),1+int(y),1+int(z))
 lines = set(map(parse, open("input.txt").read().strip().split('\n')))
 
 X=[ max([t[k]+1 for t in lines]) for k in range(3) ]
-grid=set(filter(lambda t: t not in lines,[tuple(t) for t in product(range(X[0]+1),range(X[1]+1),range(X[2]+1))]))
+grid=set([tuple(t) for t in product(range(X[0]+1),range(X[1]+1),range(X[2]+1)) if t not in lines])
 
 n,q,shell=0,[(0,0,0)],set([(0,0,0)])
 while n < len(q):
@@ -14,6 +14,6 @@ while n < len(q):
       q.append((x+dx,y+dy,z+dz)); shell.add((x+dx,y+dy,z+dz))
   n+=1
 
-dX = [[set([tuple(t[:k]+t[k+1:]) for t in filter(lambda t: t[k]==x, shell)]) for x in range(X[k]+1)] for k in range(3)]
+dX = [[set([tuple(t[:k]+t[k+1:]) for t in shell if t[k]==x]) for x in range(X[k]+1)] for k in range(3)]
 ans = sum([sum([len(dX[k][x].symmetric_difference(dX[k][x-1])) for x in range(1,X[k]+1)]) for k in range(3)])
 print(ans)
