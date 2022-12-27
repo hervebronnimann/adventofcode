11d6006215c26bbe143e42769782e6332efb26e0
Herve Bronnimann <hervebronnimann@mac.com>
2022-12-27 01:39:43 +0100

    Day 2, completed properly.  The magic of coroutines...

--- 2022/22/22.2.py
+++ 2022/22/22.2.py
@@ -1,117 +1,129 @@
 from collections import defaultdict
 import re
 
-# lines = open("example.txt").read().split('\n\n')
-lines = open("input.txt").read().split('\n\n')
+example = 0
+lines = open("example.txt" if example else "input.txt").read().split('\n\n')
 grid=lines[0].split('\n')
-print('\n'.join([ '***' + x + '***' for x in grid]))
+# print('\n'.join([ '***' + x + '***' for x in grid]))
+grid=[list(x) for x in grid]
 
 # HERE WE HAVE SOME KNOWLEDGE THAT ONLY WORKS FOR THE INPUT: IT LOOKS LIKE THIS
 #
-#            1...12....2
-#        (4L).   ..    .(5R)
-#        (4L).   ..    .(5R)
-#            1...12....2
-#            3...3 (3R)
-#        (4T).   .(2B)  
-#        (4T).   .(2B)
-#       (3L) 3...3
-#       4...45...5
-#   (1L).   ..   .(2R)
-#   (1L).   ..   .(2R)
-#       4...45...5
-#       6...6 (6R)
-#       .   .(6R)
-#       .   .(5B)
-#       6...6
+#                              1111122222
+#                              1111122222
+#         1111                 1111122222
+#         1111                 1111122222
+#         1111                 33333
+#         1111                 33333
+# 222233334444                 33333
+# 222233334444                 33333
+# 222233334444            4444455555
+# 222233334444            4444455555
+#         55556666        4444455555
+#         55556666        4444455555
+#         55556666        66666
+#         55556666        66666
+#                         66666
+#                         66666
 #
 # So it folds like this with the neighbors inverted and going to T(op) B(ottom) L(eft) or R(ight)
+# Explanation:
+# - N: side length of each cube
+# - Edge: each face is rotated clockwise,right,bottom,left,top
+# - M: map of 2D cube corners (top,left)
+# - edge(c,d): corner of cube c at beginning/end of edge d (d>0 or d<0), with direction of edge (2d) and along edge (d>0 or d<0)
+# - fold(edge1,edge2): how the 2D edges fold along each other
+
+N=0
+corresp={}
+def fold(N,edge1,edge2):
+  x1,y1,dx1,dy1,du1,dv1=edge1
+  x2,y2,dx2,dy2,du2,dv2=edge2
+  for i in range(N):
+    corresp[(x1+i*du1,y1+i*dv1,dx1,dy1)] = (x2+i*du2,y2+i*dv2,-dx2,-dy2)
+    corresp[(x2+i*du2,y2+i*dv2,dx2,dy2)] = (x1+i*du1,y1+i*dv1,-dx1,-dy1)
+
+right,bottom,left,top=1,2,3,4
+dirs={right:(1,0), bottom:(0,1), left:(-1,0), top:(0,-1)}
+cdirs={right:'>', bottom:'v', left:'<', top:'^'}
+
+def edge(N,M,c,d):
+  x,y = M[c]
+  dx,dy=dirs[abs(d)]
+  x = x*N+(N-1 if abs(d) in [right,bottom] else 0)
+  y = y*N+(N-1 if abs(d) in [bottom,left] else 0)
+  du,dv=dirs[abs(d)%4+1]
+  if d<0:
+    x,y = x+du*(N-1), y+dv*(N-1)
+    du,dv = -du,-dv
+  return x,y,dx,dy,du,dv
+
+if example:
+  N=4
+  M={1:(2,0),2:(0,1),3:(1,1),4:(2,1),5:(2,2),6:(3,2)}
+  fold(N,edge(N,M,1,left),edge(N,M,3,-top))
+  fold(N,edge(N,M,1,right),edge(N,M,6,-right))
+  fold(N,edge(N,M,4,right),edge(N,M,6,-top))
+  fold(N,edge(N,M,2,top),edge(N,M,1,-top))
+  fold(N,edge(N,M,3,bottom),edge(N,M,5,-left))
+  fold(N,edge(N,M,5,bottom),edge(N,M,2,-bottom))
+  fold(N,edge(N,M,2,left),edge(N,M,6,-bottom))
+else:
+  N=50
+  M={1:(1,0),2:(2,0),3:(1,1),4:(0,2),5:(1,2),6:(0,3)}
+  fold(N,edge(N,M,1,left),edge(N,M,4,-left))
+  fold(N,edge(N,M,2,right),edge(N,M,5,-right))
+  fold(N,edge(N,M,2,bottom),edge(N,M,3,-right))
+  fold(N,edge(N,M,3,left),edge(N,M,4,-top))
+  fold(N,edge(N,M,5,bottom),edge(N,M,6,-right))
+  fold(N,edge(N,M,6,left),edge(N,M,1,-top))
+  fold(N,edge(N,M,6,bottom),edge(N,M,2,-top))
+# print(corresp)
+
+# NOW THE ENGINE FOR MOVING ON THE CUBE, AS BEFORE
 
 inst=re.split('(\d+)', lines[1].strip())
 inst.remove('')
 inst.remove('')
-print(inst)
+# print(inst)
 
 def rot(dir,dx,dy):
   return (-dy,dx) if dir=='R' else (dy,-dx)
 
 def adv(grid,N,x,y,dx,dy):
-  if dy==0:
-    if x+dx>=0 and x+dx<len(grid[y]) and grid[y][x+dx]!=' ':
-      return (x+dx,y,dx,dy)
-    if y<N and dx==-1: # 1, connects to 4 (left)
-      assert 0<=y and x==N, f'Position ({x},{y}) direction ({dx},{dy})'
-      y=3*N-1-y; x=0; dx,dy=1,0
-    elif y<N and dx==1: # 2, connects to 5 (right)
-      assert 0<=y and x==3*N-1, f'Position ({x},{y}) direction ({dx},{dy})'
-      y=3*N-1-y; x=2*N-1; dx,dy=-1,0
-    elif y<2*N and dx==-1: # 3, connects to 4 (top)
-      assert N<=y and x==N, f'Position ({x},{y}) direction ({dx},{dy})'
-      x=y-N; y=2*N; dx,dy=0,1
-    elif y<2*N and dx==1: # 3, connects to 2 (bottom)
-      assert N<=y and x==2*N-1, f'Position ({x},{y}) direction ({dx},{dy})'
-      x=y+N; y=N-1; dx,dy=0,-1
-    elif y<3*N and dx==-1: # 4, connects to 1 (left)
-      assert 2*N<=y and x==0, f'Position ({x},{y}) direction ({dx},{dy})'
-      y=3*N-1-y; x=N; dx=1
-    elif y<3*N and dx==1: # 5, connects to 2 (right)
-      assert 2*N<=y and x==2*N-1, f'Position ({x},{y}) direction ({dx},{dy})'
-      y=3*N-1-y; x=3*N-1; dx,dy=-1,0
-    elif y<4*N and dx==-1: # 6, connects to 1 (top)
-      assert 3*N<=y and x==0, f'Position ({x},{y}) direction ({dx},{dy})'
-      x=y-2*N; y=0; dx,dy=0,1
-    elif y<4*N and dx==1: # 6, connects to 5 (bottom)
-      assert 3*N<=y and x==N-1, f'Position ({x},{y}) direction ({dx},{dy})'
-      x=y-2*N; y=3*N-1; dx,dy=0,-1
-    else:
-      assert 0<=y<=4*N, f'Position ({x},{y}) direction ({dx},{dy})'
-    return (x,y,dx,dy)
-  elif dx==0:
-    if y+dy>=0 and y+dy<len(grid) and x<len(grid[y+dy]) and grid[y+dy][x]!=' ':
-      return (x,y+dy,dx,dy)
-    if x<N and dy==-1: # 4, connects to 3 (left)
-      assert 0<=x and y==2*N, f'Position ({x},{y}) direction ({dx},{dy})'
-      y=x+N; x=N; dx,dy=1,0
-    elif x<N and dy==1: # 6, connects to 2 (top)
-      assert 0<=x and y==4*N-1, f'Position ({x},{y}) direction ({dx},{dy})'
-      x=x+2*N; y=0; dx,dy=0,1
-    elif x<2*N and dy==-1: # 1, connects to 6 (left)
-      assert N<=x and y==0, f'Position ({x},{y}) direction ({dx},{dy})'
-      y=x+2*N; x=0; dx,dy=1,0
-    elif x<2*N and dy==1: # 5, connects to 6 (right)
-      assert N<=x and y==3*N-1, f'Position ({x},{y}) direction ({dx},{dy})'
-      y=x+2*N; x=N-1; dx,dy=-1,0
-    elif x<3*N and dy==-1: # 2, connects to 6 (bottom)
-      assert 2*N<=x and y==0, f'Position ({x},{y}) direction ({dx},{dy})'
-      x=x-2*N; y=4*N-1; dx,dy=0,-1
-    elif x<3*N and dy==1: # 2, connects to 3 (right)
-      assert 2*N<=x and y==N-1, f'Position ({x},{y}) direction ({dx},{dy})'
-      y=x-N; x=2*N-1; dx,dy=-1,0
-    else:
-      assert 0<=x<=3*N, f'Position ({x},{y}) direction ({dx},{dy})'
-    return (x,y,dx,dy)
+  if x+dx>=0 and y+dy>=0 and y+dy<len(grid) and x+dx<len(grid[y+dy]) and grid[y+dy][x+dx]!=' ':
+      return (x+dx,y+dy,dx,dy)
   else:
-    assert dx==0 or dy==0
+    return corresp[(x,y,dx,dy)]
 
-x,y,dx,dy = 0,0,1,0
+# Advance to first position, and apply each instruction
+
+x,y,d,dx,dy = 0,0,right,1,0
 while x+dx<len(grid[y]) and grid[y][x]==' ': x+=dx
-N=x
-print(x,y,N)
+grid[y][x]='X'
+# print(f'Starting at {(x,y)} cube side {N}')
 
 for i in inst:
   if i=='R' or i=='L':
     # print(f'Rotate {i}')
-    dx,dy=rot(i,dx,dy)
+    dx,dy = rot(i,dx,dy)
+    d = (d%4+1 if i=='R' else (d+2)%4+1)
+    grid[y][x]=cdirs[d]
   elif i.isnumeric():
     for _ in range(int(i)):
        x2,y2,dx2,dy2=adv(grid,N,x,y,dx,dy)
        if grid[y2][x2]=='#':
          # print(f'Stop {(x,y)}')
          break
-       x,y=x2,y2
-       # print(f'Advance {(x,y)}')
+       x,y,dx,dy=x2,y2,dx2,dy2
+       for d1 in dirs: d = d1 if dirs[d1]==(dx,dy) else d
+       grid[y][x]=cdirs[d]
+       # print(f'Advance {(x,y)} dir {(dx,dy)}')
+       # print('\n'.join([ '***' + ''.join(x) + '***' for x in grid]))
   else:
     assert False, f'BAD INSTRUCTION {i}'   
 
 dir = 0 if dx==1 else 1 if dy==1 else 2 if dx==-1 else 3
+grid[y][x]='Y'
+print('\n'.join([ '***' + ''.join(x) + '***' for x in grid]))
 print(1000*(y+1)+4*(x+1)+dir)
