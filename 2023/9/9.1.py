d855067e9576d7522590b800986b6f0cb4f475ba
Herve Bronnimann <hervebronnimann@mac.com>
2023-12-08 21:39:01 -0800

    Improvements.

--- 2023/9/9.1.py
+++ 2023/9/9.1.py
@@ -1,20 +1,15 @@
-filename = "example.txt"
-filename = "input.txt"
-input = [ [ int(y) for y in x.split(" ")] for x in open(filename,'r').read().strip().split('\n') ]
+input = [ [ int(y) for y in x.split(" ")] for x in open("input.txt",'r').read().strip().split('\n') ]
 
 def diff(s): return [ s[i] - s[i-1] for i in range(1,len(s))]
 
 res = 0
 for x in input:
   y = [x]
-  while not all(z == 0 for z in x): 
-    x = diff(x)
-    y.append(x)
+  while not all(z == 0 for z in y[-1]): 
+    y.append(diff(y[-1]))
   y[-1].append(0)
   while len(y) > 1:
-    z = y[-1][-1]
-    y.pop();
+    z = y.pop()[-1]
     y[-1].append(y[-1][-1]+z)
-  print(f"Finally {y[0]}")
-  res += y[-1][-1]
+  res += y[0][-1]
 print(res)
--- 2023/9/9.2.py
+++ 2023/9/9.2.py
@@ -1,21 +1,15 @@
-filename = "example.txt"
-filename = "input.txt"
-input = [ [ int(y) for y in x.split(" ")] for x in open(filename,'r').read().strip().split('\n') ]
+input = [ [ int(y) for y in x.split(" ")] for x in open("input.txt",'r').read().strip().split('\n') ]
 
 def diff(s): return [ s[i] - s[i-1] for i in range(1,len(s))]
 
 res = 0
 for x in input:
   y = [x]
-  while not all(z == 0 for z in x): 
-    x = diff(x)
-    y.append(x)
-    print(y)
+  while not all(z == 0 for z in y[-1]): 
+    y.append(diff(y[-1]))
   y[-1].append(0)
   while len(y) > 1:
-    z = y[-1][0]
-    y.pop();
+    z = y.pop()[0]
     y[-1].insert(0,y[-1][0]-z)
-  print(f"Finally {y[0]}")
   res += y[0][0]
 print(res)
