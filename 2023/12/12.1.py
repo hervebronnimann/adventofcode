b5d1d4259f7d464c558336badb1e81360403152b
Herve Bronnimann <hervebronnimann@mac.com>
2023-12-12 09:57:34 -0800

    Cleanup

--- 2023/12/12.1.py
+++ 2023/12/12.1.py
@@ -4,19 +4,18 @@ input = open("example.txt",'r').read().strip().split('\n')
 input = open("input.txt",'r').read().strip().split('\n')
 
 def n0(x:str,n:list):
-  ret = 0
-  if len(n) == 0: ret = 1 if '#' not in x else 0
-  elif len(x) == 0: ret = 0  # because len(n)>0
-  elif len(x) < n[0]: ret = 0
-  elif len(x) == n[0]: ret = 1 if '.' not in x and len(n)== 1 else 0
-  elif x[0] == '.': ret = n0(x[1:],n)
+  if len(n) == 0: return 1 if '#' not in x else 0
+  elif len(x) == 0: return 0  # because len(n)>0
+  elif len(x) < n[0]: return 0
+  elif len(x) == n[0]: return 1 if '.' not in x and len(n)== 1 else 0
+  elif x[0] == '.': return n0(x[1:],n)
   elif x[0] == '#':
-    if '.' not in x[0:n[0]] and x[n[0]] in "?.": ret = n0(x[n[0]+1:],n[1:])
-    else: ret = 0
+    if '.' not in x[0:n[0]] and x[n[0]] in "?.":
+      return n0(x[n[0]+1:],n[1:])
+    return 0
   elif x[0]=='?':
-    ret = n0(x[1:],n) + n0("#"+x[1:],n)
-  print((x,n,ret))
-  return ret
+    return n0(x[1:],n) + n0("#"+x[1:],n)
+  return 0
     
 
 def num(x:str):
