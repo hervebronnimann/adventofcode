0bdda5af4adbac9ce289b635cf3d3a8cedd65f82
Herve Bronnimann <hervebronnimann@mac.com>
2023-12-12 22:19:09 -0800

    Cleanup

--- 2023/13/13.1.py
+++ 2023/13/13.1.py
@@ -1,25 +1,14 @@
-from functools import cache
-
-input = open("example.txt",'r').read().strip().split('\n\n')
-input = open("input.txt",'r').read().strip().split('\n\n')
+input = [ x.split('\n') for x in open("input.txt",'r').read().strip().split('\n\n') ]
 
 def solve(y:list):
     for i in range(len(y)-1):
-        found = True
-        # 0..i i+1..min(2*i,len(y)) if 
-        for j in range(min(i+1,len(y)-1-i)):
-            if y[i-j] != y[i+j+1]:
-                found = False; break
-        if found: return i+1
+        y0 = y[i::-1]
+        y1 = y[i+1:]
+        n = min(len(y0),len(y1))
+        if y0[0:n] == y1[0:n]: return i+1
     return 0
 
 def transpose(y:list):
   return ["".join(x) for x in zip(*y)]
 
-res = 0
-for x in input:
-    y = x.split('\n')
-    v = solve(y)
-    h = solve(transpose(y))
-    res += h + 100*v
-print(res)
+print( sum([100*solve(x) + solve(transpose(x)) for x in input]) )
--- 2023/13/13.2.py
+++ 2023/13/13.2.py
@@ -1,7 +1,4 @@
-from functools import cache
-
-input = open("example.txt",'r').read().strip().split('\n\n')
-input = open("input.txt",'r').read().strip().split('\n\n')
+input = [ x.split('\n') for x in open("input.txt",'r').read().strip().split('\n\n') ]
 
 def diff(x:str, y:str):
     if len(x) != len(y): return x + y
@@ -9,24 +6,13 @@ def diff(x:str, y:str):
 
 def solve(y:list):
     for i in range(len(y)-1):
-        found = 1; mis = 0
-        for j in range(min(i+1,len(y)-1-i)):
-            if y[i-j] != y[i+j+1]:
-                if found == len(diff(y[i-j],y[i+j+1]))==1:
-                    mis += 1
-                else:
-                    found = 0
-            if found==0 or mis > 1: break
-        if found == 1 and mis == 1: return i+1
+        y0 = y[i::-1]
+        y1 = y[i+1:]
+        n = min(len(y0),len(y1))
+        if sum([len(diff(u,v)) for u,v in zip(y0[0:n],y1[0:n])])==1: return i+1
     return 0
 
 def transpose(y:list):
-  return ["".join(x) for x in zip(*y)]
+    return ["".join(x) for x in zip(*y)]
 
-res = 0
-for x in input:
-    y = x.split('\n')
-    v = solve(y)
-    h = solve(transpose(y))
-    res += h + 100*v
-print(res)
+print( sum([100*solve(x) + solve(transpose(x)) for x in input]) )
