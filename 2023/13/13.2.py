39e880c4deaf8fd3c726641214aea7cc634dcd85
Herve Bronnimann <hervebronnimann@mac.com>
2023-12-12 22:00:18 -0800

    Part 2

--- 2023/13/13.2.py
+++ 2023/13/13.2.py
@@ -3,16 +3,21 @@ from functools import cache
 input = open("example.txt",'r').read().strip().split('\n\n')
 input = open("input.txt",'r').read().strip().split('\n\n')
 
+def diff(x:str, y:str):
+    if len(x) != len(y): return x + y
+    return [(i,j) for i,j in zip(x,y) if i!=j]
+
 def solve(y:list):
-    print(y)
     for i in range(len(y)-1):
-        found = True
-        # 0..i i+1..min(2*i,len(y)) if 
+        found = 1; mis = 0
         for j in range(min(i+1,len(y)-1-i)):
-            print(f"Check {i}: {i-j} {i+1+j}")
             if y[i-j] != y[i+j+1]:
-                found = False; print(f"Differ {i-j} {i+1+j}"); break
-        if found: print(f"Found {i}"); return i+1
+                if found == len(diff(y[i-j],y[i+j+1]))==1:
+                    mis += 1
+                else:
+                    found = 0
+            if found==0 or mis > 1: break
+        if found == 1 and mis == 1: return i+1
     return 0
 
 def transpose(y:list):
