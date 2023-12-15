cb754465e1557c8072c49f6eee02edfeb4a2bec5
Herve Bronnimann <hervebronnimann@mac.com>
2023-12-14 21:54:23 -0800

    Further cleanup - boring problem today

--- 2023/15/15.2.py
+++ 2023/15/15.2.py
@@ -1,17 +1,11 @@
-# example = open("example.txt",'r').read().strip().split(',')
 input = open("input.txt",'r').read().strip().split(',')
 
 def HASH(x: str):
     res = 0;
-    for c in x:
-        res += ord(c); res *= 17; res %= 256
+    for c in x: res += ord(c); res *= 17; res %= 256
     return res
 
-# print([HASH(x[0:2]) for x in example])
-
-Boxes = []
-for i in range(256):
-    Boxes.append(list())
+Boxes = [ list() for _ in range(256) ]
 
 for x in input:
     if x[-1] == '-':
@@ -23,19 +17,12 @@ for x in input:
     else:
         label,n = x.split('=')
         box = HASH(label); n = int(n)
-        # print(label, box, Boxes[box])
         found = False
-        for i,(z,m) in enumerate(Boxes[box]):
+        for i,(z,_) in enumerate(Boxes[box]):
             if z==label:
                 Boxes[box][i] = (label,n)
-                found = True
-                break
+                found = True; break
         if not found:
             Boxes[box].append( (label,n) )
-    # print(x,[(i,b) for i,b in enumerate(Boxes) if len(b)>0])
 
-res = 0
-for b,box in enumerate(Boxes):
-  for  i,(_,n) in enumerate(box):
-    res += (b+1) * (i+1) * n
-print(res)
+print(sum([sum([(b+1) * (i+1) * n for i,(_,n) in enumerate(box)]) for b,box in enumerate(Boxes)]))
