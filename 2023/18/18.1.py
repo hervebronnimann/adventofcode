c36ba6cec16e0e03e4054ecedc2280fe794c325d
Herve Bronnimann <hervebronnimann@mac.com>
2023-12-18 01:16:11 -0500

    Cleanup

--- 2023/18/18.1.py
+++ 2023/18/18.1.py
@@ -5,12 +5,12 @@ dir = { 'R':0, 'D':1, 'L':2, 'U':3 }
 diri = [ 0, 1, 0, -1 ]
 dirj = [ 1, 0, -1, 0 ]
 
-# Graph of (i,j)
+# Polygon, points on the boundary
 i,j= (0,0)
-p = [(i,j)]; L = 0
+p = [(i,j)]; boundary = 0
 for x in input:
     d,n,_ = x.split(' '); n = int(n)
-    L += n
+    boundary += n
     i += n*diri[dir[d]]
     j += n*dirj[dir[d]]
     p.append((i,j))
@@ -21,5 +21,6 @@ for i in range(len(p)):
     x2,y2 = p[(i+1) % len(p)]
     area += x2 * y1 - x1 * y2
 
-inside = (area - L)//2 + 1
+# Pick's theorem:  area = inside + boundary / 2 - 1
+inside = (area - boundary)//2 + 1
 print( L + inside )
--- 2023/18/18.2.py
+++ 2023/18/18.2.py
@@ -5,14 +5,14 @@ dir = { 'R':0, 'D':1, 'L':2, 'U':3 }
 diri = [ 0, 1, 0, -1 ]
 dirj = [ 1, 0, -1, 0 ]
 
-# Graph of (i,j)
+# Polygon, points on the boundary
 i,j= (0,0)
-p = [(i,j)]; L = 0
+p = [(i,j)]; boundary = 0
 for x in input:
     _,_,h = x.split(' ')
     d = int(h[-2])
     n = int(h[2:-2],16)
-    L += n
+    boundary += n
     i += n*diri[d]
     j += n*dirj[d]
     p.append((i,j))
@@ -23,5 +23,6 @@ for i in range(len(p)):
     x2,y2 = p[(i+1) % len(p)]
     area += x2 * y1 - x1 * y2
 
-inside = (area - L)//2 + 1
+# Pick's theorem:  area = inside + boundary / 2 - 1
+inside = (area - boundary)//2 + 1
 print( L + inside )
