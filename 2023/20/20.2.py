99eaaab1bb004056ee49f1eb7f36a9da29b2e815
Herve Bronnimann <hervebronnimann@mac.com>
2023-12-25 21:55:11 +0900

    Day 20, part 2, with a bit of help from the internet.

--- 2023/20/20.2.py
+++ 2023/20/20.2.py
@@ -1,10 +1,11 @@
 from collections import deque
 import re
+import math
 input = open("input.txt",'r').read().strip().split('\n\n')
 
 modules = { 'rx' : ('>',[]) }
 state = { 'rx' : 0, 'rxh' : 0 }
-inputs = { 'rx': set() }
+endpoints = []  # those nodes who feed into the final '&vr -> rx'
 
 for config in input[0].split('\n'):
     config = re.sub('[,-> ]+', ' ', config).strip()
@@ -12,29 +13,27 @@ for config in input[0].split('\n'):
     m = x[0][1:] if x[0] != 'broadcaster' else 'broadcaster'
     type = x[0][0] if x[0] != 'broadcaster' else '='
     modules[m] = (type, x[1:])
-    print(m, type, x[1:])
-    inputs[m] = set()
+    # print(m, type, x[1:])
     if type == '%': state[m] = 'off'
     if type == '&': state[m] = dict()
+    if 'vr' in x[1:]: endpoints.append(m)
 
 # Build initial state, for conjunction modules
 for m in modules:
     _,dest = modules[m]
     for d in dest:
-        inputs[d].add(m)
         if isinstance(state[d], dict):
             state[d][m] = 'low'
 
 # Propagate a signal:
 b = { 'low':0, 'high':0 }
-contrib = { x: set() for x in modules }
-def push_button():
+cycles = { x:[] for x in endpoints }
+def push_button(k):
     sig = deque([('broadcaster','low','button')])
-    state['rx'] = 0
-    state['rxh'] = 0
+    state['rx'] = 0 ; p = 0
     while sig:
         m,s,i = sig.popleft()
-        b[s] += 1
+        b[s] += 1; p += 1
         type,dest = modules[m]
         s2 = None
         if type == '=':
@@ -45,24 +44,23 @@ def push_button():
         if type == '&':
             state[m][i] = s
             s2 = 'low' if all(x == 'high' for x in state[m].values()) else 'high'
+            if m in endpoints and s2 == 'high':
+                cycles[m].append((k,p))
         if type == '>' and s == 'low':
             state['rx'] += 1
-        if type == '>' and s == 'high':
-            state['rxh'] += 1
         if s2:
-            for d in dest:
-                contrib[d].update(contrib[m])
-                sig.append((d,s2,m))
+            for d in dest: sig.append((d,s2,m))
     return (b['low'],b['high'])
 
-# This takes forever - I've had it run till 155M and not found...
-def brute_force(N):
-    n = 0
-    while state['rx']!=1 and n <= N:
-        n+=1; push_button();
-        print(f'After {n}, rx received {state["rx"]} low signals and {state["rxh"]} high signals')
+# Brute force takes forever - I've had it run till 155M and not found...
+# So looking a bit deeper, found that vr->rx has four ancestors (call them endpoints),
+# each of them needing to be high, and looking at their cyclicities,
+# they tick exactly at multiples of cycles.
+def cycle_detect(N):
+    for n in range(1,N+1):
+        push_button(n);
         if n == 1000: print(f"Part 1: {b['low']*b['high']}")
-    return n
 
-brute_force(100000)
-print(contrib)
+cycle_detect(10000)
+# print(cycles)
+print(f'Part 2: {math.prod(x[0][0] for x in cycles.values())}')
