f8e27040d909cc53895c65aace721618102ed98b
Herve Bronnimann <hervebronnimann@mac.com>
2023-12-27 13:23:50 +0900

    Some things are working, but this is so bullshit.

--- 2023/23/23.1.py
+++ 2023/23/23.1.py
@@ -6,6 +6,7 @@ n = len(input); m = len(input[0])
 
 # 0,1,2,3 -> R,D,L,U
 dir = { 'R':0, 'D':1, 'L':2, 'U':3 }
+dirc = [ '>', 'v', '<', '^' ]
 diri = [ 0, 1, 0, -1 ]
 dirj = [ 1, 0, -1, 0 ]
 
@@ -19,20 +20,55 @@ def neighbors(i,j):
     return L
 
 # Compute the junctions, and check that the other parts are linear.
-V = [ (0,1), (n-1,m-2) ]
+V = [ (0,1,1) ] # , (n,m-2,1) ]
 for i in range(n):
     for j in range(m):
         if input[i][j] != '.': continue
         if sum([1 if input[x][y] in '><v^' else 0 for x,y in neighbors(i,j)]) > 1:
-            input[i][j]=chr(ord('A')+len(V)-2)
-            V.append((i,j))
+            # input[i][j]=chr(ord('A')+len(V)-2)
+            for d in range(4):
+                if dirc[d] == input[i+diri[d]][j+dirj[d]]: V.append((i,j,d))
         elif sum([1 if input[x][y]=='.><v^' else 0 for x,y in neighbors(i,j)]) > 2:
             print(f'Vertex {i},{j} a junction without clear ><v^ markers')
 print(V)
 print("\n".join([ "".join(x) for x in input]))
 
-# Compute distances between the junctions, while obeying
-D = { v:0 for v in V }
-Q = deque(V); visited = set(V)
-# while len(Q)>0:
+# Going from i,j into direction d, where is the next tile?
+def find_next(i,j,d0):
+    u,v = i+diri[d0], j+dirj[d0]
+    for d in range(4):
+        if u+diri[d] not in range(n): continue
+        if v+dirj[d] not in range(m): continue
+        if input[u+diri[d]][v+dirj[d]] == '#': continue
+        if (u+diri[d],v+dirj[d]) == (i,j): continue
+        if u+diri[d] == n: return(u,v,d)
+        if input[u+diri[d]][v+dirj[d]] == '.': return(u,v,d)
+        # For safety, check that we slide in the correct direction
+        if dirc[d] == input[u+diri[d]][v+dirj[d]]: return(u,v,d)
+    return None
 
+# Starting at junction i,j in direction d, where is the next junction, and how far?
+def find_next_junction(i,j,d):
+    k = 1
+    while True:
+        w = find_next(i,j,d)
+        if not w: return (None,None,k)
+        i,j,d = w
+        k += 1
+        if input[i][j] != '.': break
+    return (i+diri[d],j+dirj[d],k+1)
+
+# Exploration:  make sure all of the above works
+for i,j,d in V:
+    for e in range(4):
+        u,v = i+diri[d], j+dirj[d]
+        if input[u+diri[e]][u+dirj[e]] == '.':
+            u,v,k = find_next_junction(u,v,e)
+            if u:
+                print(f'From {i},{j} going {d}, {k} steps to {u},{v}')
+            else:
+                print(f'From {i},{j} going {d}, {k} steps to dead end')
+
+
+# Compute distances between the junctions, while obeying
+src = (0,1); dst = (n,m-2)
