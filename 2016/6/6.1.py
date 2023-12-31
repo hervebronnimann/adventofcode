8afef952fa1884aaeb29d006bc796a5f95ff2df2
Herve Bronnimann <hervebronnimann@mac.com>
2023-12-31 15:39:53 +0900

    Day 6 of 2016 - trivial using python!

---
+++ 2016/6/6.1.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+input = open("input.txt",'r').read().strip().split('\n')
+
+res = ''
+for i in range(len(input[0])):
+    res += Counter([x[i] for x in input]).most_common()[0][0]
+print(res)
