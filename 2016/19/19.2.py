c38978912ccfba0d8b08975aed3c82d88b8415f0
Herve Bronnimann <hervebronnimann@mac.com>
2024-01-06 01:36:59 -0800

    Reworking with an idea...

--- 2016/19/19.2.py
+++ 2016/19/19.2.py
@@ -1,4 +1,4 @@
-# Josephus problem
+from collections import deque
 
 N = 5    # Should return 2
 N = 50   # Should return 23
@@ -6,7 +6,7 @@ N = 51   # Should return 24
 N = 52   # Should return 23
 N = 3004953
 
-# NAIVE SOLUTION:
+# NAIVE SOLUTION (and incorrect for values < 5): too slow because removes elements one by one, and pop is inefficient!
 def j2(N):
     x = list(range(1,N+1))
     n = 0
@@ -18,10 +18,21 @@ def j2(N):
     # from the example, we know the last elf is the one next to the current position
     return x[(n+1)%len(x)]
 
-# WOW Super interesting sequence, closed form solution
-# print([(N,j2(N)) for N in range(1,1000)])
+# LINEAR TIME SOLUTION: not as slow because removes elements one by one but pop is made efficient
+def j2bis(N):
+    x,y = deque(range(1,N//2+1)),deque(range(N//2+1,N+1))
+    while x:
+        y.popleft()
+        y.append(x.popleft())
+        if len(x)<len(y)-1: x.append(y.popleft())
+    return y[0]
 
-# [(1, 1), (2, 2), (3, 2),
+print(j2bis(N))
+
+# WOW Super interesting sequence, closed form solution
+print([(N,j2bis(N)) for N in range(1,1000)])
+
+# [(1, 1), (2, 1), (3, 2),
 # (4, 1), (5, 2), (6, 3), *** (7, 5), (8, 7), (9, 9),
 # (10, 1), (11, 2), (12, 3), (13, 4), (14, 5), (15, 6), (16, 7), (17, 8), (18, 9), *** (19, 11), (20, 13), (21, 15), (22, 17), (23, 19), (24, 21), (25, 23), (26, 25), (27, 27),
 # (28, 1), (29, 2), (30, 3), (31, 4), (32, 5), (33, 6), (34, 7), (35, 8), (36, 9), (37, 10), (38, 11), (39, 12), (40, 13), (41, 14), (42, 15), (43, 16), (44, 17), (45, 18), (46, 19), (47, 20), (48, 21), (49, 22), (50, 23), (51, 24), (52, 25), (53, 26), (54, 27), *** (55, 29), (56, 31), (57, 33), (58, 35), (59, 37), (60, 39), (61, 41), (62, 43), (63, 45), (64, 47), (65, 49), (66, 51), (67, 53), (68, 55), (69, 57), (70, 59), (71, 61), (72, 63), (73, 65), (74, 67), (75, 69), (76, 71), (77, 73), (78, 75), (79, 77), (80, 79), (81, 81),
