312fc5e39431cf9a33b409b5ef75c9b6040ebd9d
Herve <herve@tgsmc.com>
2024-12-04 23:39:46 -0800

    Part 1 day 6

---
+++ 2024/05/5.1.py
@@ -0,0 +1,15 @@
+text = open("input.txt",'r').read().strip().split('\n')
+rules = [ tuple(t.split('|'))  for t in text if '|' in t ]
+pages = [ t.split(',')  for t in text if ',' in t ]
+
+print(rules)
+print(pages)
+
+def good(p):
+    for i in range(len(p)):
+        for j in range(i+1,len(p)):
+            if (p[j],p[i]) in rules: return 0
+    return int(p[len(p)//2])
+
+print(sum([good(p) for p in pages]))
+
